ac78f1e54060c8ff98455b987651f011fea44257
Margarita <mv@asofts.ru>
2020-06-10 17:45:04 +0700

    render html pages from django, includes of navbar, footer, meta

--- front/urls.py
+++ front/urls.py
@@ -4,4 +4,14 @@ from front import views
 
 urlpatterns = [
     path('', views.Index.as_view()),
+    path('about', views.About.as_view()),
+    path('project/<int:project_id>', views.Project.as_view()),
+    path('blagodarnosti', views.Blagodarnosti.as_view()),
+    path('news/<int:category_id>', views.News.as_view()),
+    path('article/<int:category_id>-<int:article_id>', views.Article.as_view()),
+    path('documents', views.Documents.as_view()),
+    path('rehab-program', views.RehabProgram.as_view()),
+    path('rehab-zavisimosti', views.RehabVidyZavisimosti.as_view()),
+    path('rehab-sozavisimost', views.RehabSozavisimost.as_view()),
+    path('rehab-pamyatki', views.RehabPamyatki.as_view()),
 ]
--- front/views.py
+++ front/views.py
@@ -6,4 +6,63 @@ from django.views import View
 
 class Index(View):
     def get(self, request):
+        #todo бегущая строка settings/home
+        #todo текст о нас
+        #todo проекты с выбором фона, лого, заголовка и текста
+        #todo три последние новости всех категорий кроме реабилитации Lenta
+        #todo телефон, почта, адрес из settings
+        #todo реквизиты
         return render(request, 'index.html', context={})
+
+
+class About(View):
+    def get(self, request):
+        # todo возможно подгрузка фото из базы
+        return render(request, 'about.html', context={})
+
+class Project(View):
+    def get(self, request, project_id):
+        # todo подгрузка фото из базы, лого, названия и описания с цветом (для главной)
+        context = {'project_id': project_id,
+                   'photos':[], 'description':''}
+        return render(request, 'project.html', context={})
+
+class Blagodarnosti(View):
+    def get(self, request):
+        # todo подгрузка фото из базы по годам
+        return render(request, 'blagodarnosti.html', context={})
+
+
+class News(View):
+    def get(self, request, category_id):
+        # todo функционал
+        # todo функционал под реабилитацию
+        return render(request, 'news.html', context={})
+        # todo return render(request, 'reability_news.html', context={})
+
+class Article(View):
+    def get(self, request, category_id, article_id):
+        # todo функционал
+        # todo функционал под реабилитацию
+        return render(request, 'article.html', context={})
+
+class Documents(View):
+    def get(self, request):
+        # todo функционал добавления доков в базу по дате
+        return render(request, 'documents.html', context={})
+
+class RehabProgram(View):
+    def get(self, request):
+        return render(request, 'reability.html', context={})
+
+class RehabVidyZavisimosti(View):
+    def get(self, request):
+        return render(request, 'vidy_zavisimosti.html', context={})
+
+class RehabSozavisimost(View):
+    def get(self, request):
+        return render(request, 'sozavisimost.html', context={})
+
+class RehabPamyatki(View):
+    def get(self, request):
+        return render(request, 'pamyatki.html', context={})
