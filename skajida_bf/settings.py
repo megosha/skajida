7a66540769330f4c5c0e12575a5a9e4c6bb004eb
Margarita <mv@asofts.ru>
2020-06-26 11:29:06 +0700

    stupid fix

--- skajida_bf/settings.py
+++ skajida_bf/settings.py
@@ -130,7 +130,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_DIRS = [os.path.join(BASE_DIR, 'skajida_bf', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'skajida_bf', 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
