85b954828ee4d3c41e47b7e13328e3a117bc40d9
Margarita <mv@asofts.ru>
2020-06-29 18:05:33 +0700

    incorrect static directory  fix

--- skajida_bf/static/static/custom.js
+++
@@ -1,9 +0,0 @@
-function valid_name(id) {
-    inp = document.getElementById(id);
-    inp.value = inp.value.replace(/[0-9++=~`!@#$%\^&*()_|\/№\;:?<>,."'{}\[\]]/, '');
-}
-
-function valid_phone(id) {
-    field = document.getElementById(id);
-    field.value = field.value.replace(/[^0-9aA-zZаА-яЯ)( @.+-]+/g, '');
-}
