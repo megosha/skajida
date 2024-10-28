5a60270aefe0c24a75f78bf88954bf6d49b49838
Есин Андрей Павлович (4102371) <apesin@dev.vtb.ru>
2024-10-28 15:57:18 +0700

    urls

--- front/urls.py
+++ front/urls.py
@@ -2,20 +2,20 @@ from django.urls import re_path, path, include
 
 from front import views, ajax
 urlpatterns = [
-    path('', views.Index.as_view()),
-    path('about', views.About.as_view()),
+    re_path('', views.Index.as_view()),
+    re_path('about/?', views.About.as_view()),
     path('project/<int:project_id>', views.Project.as_view()),
-    path('blagodarnosti', views.Blagodarnosti.as_view()),
+    re_path('blagodarnosti/?', views.Blagodarnosti.as_view()),
     path('news/<int:category_id>', views.News.as_view()),
     path('article/<int:category_id>-<int:article_id>', views.Article.as_view()),
-    path('documents', views.Documents.as_view()),
-    path('rehab-program', views.RehabProgram.as_view()),
-    path('rehab-zavisimosti', views.RehabVidyZavisimosti.as_view()),
-    path('rehab-sozavisimost', views.RehabSozavisimost.as_view()),
-    path('rehab-pamyatki', views.RehabPamyatki.as_view()),
-    path('feedback/', ajax.Feedback.as_view()),
-    path('login/', views.Login.as_view()),
-    path('othere/', views.OthereRender.as_view()),
-    path('taxi/', views.TaxiRender.as_view()),
+    re_path('documents/?', views.Documents.as_view()),
+    re_path('rehab-program/?', views.RehabProgram.as_view()),
+    re_path('rehab-zavisimosti/?', views.RehabVidyZavisimosti.as_view()),
+    re_path('rehab-sozavisimost/?', views.RehabSozavisimost.as_view()),
+    re_path('rehab-pamyatki/?', views.RehabPamyatki.as_view()),
+    re_path('feedback/?', ajax.Feedback.as_view()),
+    re_path('login/?', views.Login.as_view()),
+    re_path('othere/?', views.OthereRender.as_view()),
+    re_path('taxi/?', views.TaxiRender.as_view()),
 ]
 
